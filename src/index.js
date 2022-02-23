ae37db048b5cc572375b114840525fdb5b1dfcc3
Bless Ambele <blessambel1@gmail.com>
2022-02-23 13:37:24 -0930

    implementing redux

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("Hello World!");
+import store from "./store";
+import { BUG_ADDED, BUG_REMOVED } from "./actionTypes";
+
+const unsubscribe = store.subscribe(() => {
+  console.log("store changed", store.getState());
+});
+
+store.dispatch({
+  type: BUG_ADDED,
+  payload: {
+    description: "Bug1",
+  },
+});
+unsubscribe();
+
+store.dispatch({
+  type: BUG_REMOVED,
+  payload: {
+    id: 1,
+  },
+});
+
+console.log(store.getState());
