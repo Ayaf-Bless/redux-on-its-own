a5b4eae2d64bdf4779d7192035c81b12eae220dd
Bless Ambele <blessambel1@gmail.com>
2022-02-23 15:39:36 -0930

    using the createAction from reduxToolkit

--- src/index.js
+++ src/index.js
@@ -7,10 +7,22 @@ const unsubscribe = store.subscribe(() => {
   console.log("confStore changed", store.getState());
 });
 
-store.dispatch(bugAdded("Bug1"));
-store.dispatch(bugAdded("Bug2"));
-store.dispatch(bugAdded("Bug3"));
+store.dispatch(bugAdded({ description: "Bug1" }));
+store.dispatch(
+  bugAdded({
+    description: "Bug2",
+  })
+);
+store.dispatch(
+  bugAdded({
+    description: "Bug3",
+  })
+);
 
-store.dispatch(bugResolved(1));
+store.dispatch(
+  bugResolved({
+    id: 1,
+  })
+);
 
 console.log(store.getState());
--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,28 +1,19 @@
+import { createAction } from "@reduxjs/toolkit";
 // ACTION TYPES
 const BUG_ADDED = "bugAdded";
 const BUG_REMOVED = "bugRemoved";
 const BUG_RESOLVED = "bugResolved";
 
 //ACTIONS CREATORS
-
-export const bugAdded = (description) => ({
-  type: BUG_ADDED,
-  payload: {
-    description,
-  },
-});
-
-export const bugResolved = (id) => ({
-  type: BUG_RESOLVED,
-  payload: {
-    id,
-  },
-});
+export const bugUpdated = createAction("BugUpdated");
+export const bugAdded = createAction("BugUpdated");
+export const bugResolved = createAction("BugResolve");
+export const bugRemoved = createAction("BugRemoved");
 
 let lastId = 0;
 
 export default function reducer(state = [], action) {
-  if (action.type === BUG_ADDED)
+  if (action.type === bugAdded.type)
     return [
       ...state,
       {
@@ -31,9 +22,9 @@ export default function reducer(state = [], action) {
         resolved: false,
       },
     ];
-  else if (action.type === BUG_REMOVED)
+  else if (action.type === bugRemoved.type)
     return state.filter((bug) => bug.id !== action.payload.id);
-  else if (action.type === BUG_RESOLVED)
+  else if (action.type === bugResolved.type)
     return state.map((bug) =>
       bug.id !== action.payload.id ? bug : { ...bug, resolved: true }
     );
