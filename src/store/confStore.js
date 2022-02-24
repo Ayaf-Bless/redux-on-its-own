37f4d1d543b61318983d437c477220a21e19bdd9
Bless Ambele <blessambel1@gmail.com>
2022-02-23 15:26:13 -0930

    adding redux-toolkit to the mix

--- src/store/confStore.js
+++ src/store/confStore.js
@@ -1,7 +1,6 @@
-import { createStore } from "redux";
-import { devToolsEnhancer } from "redux-devtools-extension";
+import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./bugs";
 
 export default function confStore() {
-  return createStore(reducer, devToolsEnhancer({ trace: true }));
+  return configureStore({ reducer });
 }
