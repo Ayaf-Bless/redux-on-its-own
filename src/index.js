1dc81b578870af90c313d7dc785072a11cf68c88
Bless Ambele <blessambel1@gmail.com>
2022-03-09 06:45:05 -0930

    crating my own func middleware

--- src/index.js
+++ src/index.js
@@ -4,33 +4,7 @@ import { projectAdded } from "./store/project";
 
 const store = confStore();
 
-const unsubscribe = store.subscribe(() => {
-  console.log("confStore changed", store.getState());
+store.dispatch((dispatch, getState) => {
+  dispatch({ type: "bugAdded", bugs: [1, 2, 3] });
+  console.log(getState());
 });
-
-store.dispatch(
-  projectAdded({
-    name: "project 1",
-  })
-);
-
-store.dispatch(bugAdded({ description: "Bug1" }));
-store.dispatch(
-  bugAdded({
-    description: "Bug2",
-  })
-);
-store.dispatch(
-  bugAdded({
-    description: "Bug3",
-  })
-);
-
-store.dispatch(
-  bugResolved({
-    id: 1,
-  })
-);
-const unresolved = unresolvedBugsSelector(store.getState());
-console.log(unresolved);
-console.log(store.getState());
--- src/store/confStore.js
+++ src/store/confStore.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./reducer";
 import { logger } from "./middleware/logger";
+import { func } from "./middleware/func";
 
 export default function confStore() {
-  return configureStore({ reducer, middleware: [logger("console")] });
+  return configureStore({ reducer, middleware: [logger("console"), func] });
 }
